c2b543ebc2f9e4629d773b52e2c75e4fd2a39f8e
Ilya Nikiforov <ilnik1@yandex.ru>
2024-03-09 09:17:53 +0400

    asser testing

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,5 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
+print("All is good!")
